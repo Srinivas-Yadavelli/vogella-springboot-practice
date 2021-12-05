1b8a0c6385e99d71da6a36b06b9d4e08fab51a37
Srinivas-Yadavelli <techseen9@gmail.com>
2021-12-05 16:14:21 +0530

    Intial commit

---
+++ src/main/java/org/springboot/practice/VogellaSpringbootPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VogellaSpringbootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VogellaSpringbootPracticeApplication.class, args);
+	}
+
+}
